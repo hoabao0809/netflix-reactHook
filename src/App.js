623738c16c47057f6d966b177dfe48a4b6e8b68e
hoabao0809 <hoabao.0809@gmail.com>
2022-11-06 12:14:34 +0700

    add profile page navigation (useNavigate instead useHistory)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
   const user = useSelector(selectUser);
@@ -16,7 +17,7 @@ function App() {
       if (userAuth) {
         dispatch(login({ uid: userAuth.uid, email: userAuth.email }));
       } else {
-        dispatch(logout);
+        dispatch(logout());
       }
     });
 
@@ -30,6 +31,7 @@ function App() {
           <LoginScreen />
         ) : (
           <Routes>
+            <Route exact path="/profile" element={<ProfileScreen />} />
             <Route exact path="/" element={<HomeScreen />} />
           </Routes>
         )}
